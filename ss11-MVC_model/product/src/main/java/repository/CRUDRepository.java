7a0e5e1f525d68ec4911217c3d7f7b1777318db3
pacry241 <phanduyman241@gmail.com>
2022-09-25 05:43:03 +0700

    upload

---
+++ ss11-MVC_model/product/src/main/java/repository/CRUDRepository.java
@@ -0,0 +1,19 @@
+package repository;
+
+import model.Product;
+
+import java.util.List;
+
+public interface CRUDRepository<T> {
+    List<T> findAll();
+
+    void create(T t) throws Exception;
+
+    void update(T t) throws Exception;
+
+    void delete(String id) throws Exception;
+
+    Product findById(String id) throws Exception;
+
+    List<T> findByName(String name) throws Exception;
+}
